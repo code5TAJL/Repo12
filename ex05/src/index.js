c1aea925ca7705337507042812885bacc59b2a71
code5TAJL <asmir.sulejmanovic@outlook.com>
2022-04-08 19:24:26 +0200

    Added ex05/day12

--- ex05/src/index.js
+++ ex05/src/index.js
@@ -1,8 +1,31 @@
 // Only change code below this line
+var myPetsArray = [
+    {
+        animalType: "Dog",
+        name: "Pujdo"
+    },
 
+    {
+        animalType: "Cat",
+        name: "Maca"
+    },
+
+    {
+        animalType: "Bird",
+        name: "Tweety"
+    }
+];
+
+function myPetsFunction(pets) {
+    
+    var myNewPets = myPetsArray[1].name;
+    return myNewPets;
+}
 // Only change code above this line
 
-console.log(myPetsFunction()); // Change this line
-
+console.log(myPetsFunction(myPetsArray));  // Change this line
 module.exports = myPetsFunction;
 module.exports.myPets = myPetsArray;
+
+
+
