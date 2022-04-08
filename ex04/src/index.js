607974e8f5fcd864e8b8e8b9369a1be5019c6f51
code5TAJL <asmir.sulejmanovic@outlook.com>
2022-04-08 18:52:40 +0200

    Added ex04/day12

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -1,6 +1,25 @@
 // Only change code below this line
-
+function sumFibonacci(num) {
+    var fib0 = 0;
+    var fib1 = 1;
+    var fibonacciArray = 1;
+    var sum = fib0;
+    while (fibonacciArray <= num) {
+        if (fibonacciArray % 2) {
+            sum = sum + fib1;
+        }
+        fibonacciArray = fib0 + fib1;
+        fib1 = fib1 +fib0;
+        fib0 = fib1 - fib0;
+    }
+    return sum;
+}
 // Only change code above this line
 
-console.log(sumFibonacci(num)); // Change this line
+console.log(sumFibonacci(1)); // Change this line
+console.log(sumFibonacci(10));
+console.log(sumFibonacci(20));
+console.log(sumFibonacci(4));
+console.log(sumFibonacci(-5));
 module.exports = sumFibonacci;
+
